b4df9ca5c038b6cccd4c4a731625c33e3202a8da
mlpmustafa <121035609+mlpmustafa@users.noreply.github.com>
2023-01-31 16:02:22 +0300

    Update yolov_5_get_predict.py

--- yolov_5_get_predict.py
+++ yolov_5_get_predict.py
@@ -10,3 +10,10 @@ for root, _, files in os.walk(r'D:\yolo\yolov5\data\dmr\test\\'):
         if file.endswith('.png'):
             results = model(root + '\\' + file)
             results.save(save_dir = 'preds/img')  # or .show(), .save(), .crop(), .pandas(), etc.
+            
+            """"
+            predicted_img_with_box = results.render()
+            predicted_img_class = results.pandas().xywh[0]['name'][0]
+            results.pandas().xywh[0].name
+            results.save(save_dir = 'cbis_preds_2/img')  # or .show(), .save(), .crop(), .pandas(), etc.
+            """
